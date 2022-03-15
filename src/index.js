23373c37ef5f5b4c8b09b2703654f699f983b9c1
RomainBoudet <romainoclock@gmail.com>
2022-03-15 16:24:56 +0100

    Fisrt composant done !

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import {sum, mul, substract} from './math';
-
-
-console.log(sum(4, 8));
-console.log(mul(9,9));
-console.log(substract(5, 2));
-console.log('Coucou, ca tourne sur webserver !!');
-//console.log(substract(4, 10));
-//console.log(mul(8,8));
+import React from 'react';
+import {render} from 'react-dom';
 
 
 
+const Hello = () => <div>
+Hello REACT !!</div>;
+
+
+const root = document.getElementById('root');
+
+render (<Hello/>, root);
